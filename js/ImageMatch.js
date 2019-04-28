758973e00509cca2ee95a57647f9a4b5d3c4a024
root <kevinw326@gmail.com>
2019-04-28 01:56:12 -0400

    testing

--- js/ImageMatch.js
+++ js/ImageMatch.js
@@ -1,15 +1,6 @@
+var fs = require('fs');
 var folder = "../images/";
 
 function matchDims(dimRatio){
-  var bound = dimRatio * 0.1
-  $.ajax({
-      url : folder,
-      success: function (data) {
-          $(data).find("a").attr("href", function (i, val) {
-              if( val.match(/\.(svg)$/) ) {
-                  console.log(folder + val)
-              }
-          });
-      }
-  });
+  var files = fs.readdirSync(folder);
 }
